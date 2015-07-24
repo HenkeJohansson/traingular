8c48dc563251fc4634caa157e7ee753f1f424946
Henrik Johansson <henrik89johansson@gmail.com>
2015-07-24 23:59:58 +0200

    No create json

--- api/getExercises.php
+++ api/getExercises.php
@@ -6,35 +6,18 @@ include 'includes/connect.inc.php';
 $sql = "SELECT * FROM exercieses";
 $result = $db->query($sql);
 
-?>
 
-<div style="width:80%;margin:40px auto;">
-	<table border="1">
-		<thead>
-			<tr>
-				<th>Namn</th>
-				<th>Reps/Sets</th>
-				<th>Instruktion</th>
-				<th>Muskelgrupp</th>
-			</tr>
-		</thead>
-		<tbody>
+$exercieses = array();
 
-			<?php foreach ($result as $row) :
-				$name = $row['name'];
-				$reps = $row['reps'];
-				$sets = $row['sets'];
-				$desc = $row['description'];
-				$muscleGroup = $row['muscleGroup']; ?>
-			<tr>
+foreach ($result as $row) {
+	$name = $row['name'];
+	$reps = $row['reps'];
+	$sets = $row['sets'];
+	$desc = $row['description'];
+	$muscleGroup = $row['muscleGroup'];
+}
 
-				<td><?php echo $name; ?></td>
-				<td><?php echo $reps . ' - ' . $sets; ?></td>
-				<td><?php echo $desc; ?></td>
-				<td><?php echo $muscleGroup; ?></td>
-			</tr>
-			<?php endforeach; ?>
-
-		</tbody>
-	</table>
-</div>
+// $result = array('muscleGroup' => $exercises);
+echo '<pre>';
+var_dump($exercieses);
+echo '</pre>';
